c89b16ba5e1742e88b985e37b2d5bd1615bc3400
tot3110 <abasit3110@gmail.com>
2025-10-09 10:36:39 +0500

    Add Components in home page

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './Home/Home'
+import About from './About/About'
+const App = () => {
+  return (
+    <>
+      <Home />
+      <About />
+    </>
+  )
+}
+
+export default App
